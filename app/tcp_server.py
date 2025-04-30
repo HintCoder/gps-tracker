6d78ee0563a3b9aa2b193a72aae0c755f8aa232b
Gabriel <gabrieldlucas@gmail.com>
2025-04-30 12:02:39 -0300

    feat: save parsed location data into database

--- app/tcp_server.py
+++ app/tcp_server.py
@@ -1,9 +1,23 @@
 import socketserver
+from .parser import parse_location_packet
+from .database import SessionLocal
+from .models import Location
 
 class GPSHandler(socketserver.BaseRequestHandler):
     def handle(self):
         data = self.request.recv(1024)
-        print(f"Received data: {data.hex()}")
+        parsed = parse_location_packet(data)
+        
+        if parsed:
+            db = SessionLocal()
+            #TODO: fazer com update
+            db.query(Location).filter(Location.device_id == parsed["device_id"]).delete()
+            db.add(Location(**parsed))
+            db.commit()
+            db.close()
+        else:
+            print("Invalid packet received")
+            
         
 if __name__ == "__main__":
     server = socketserver.TCPServer(("0.0.0.0", 9000), GPSHandler)
